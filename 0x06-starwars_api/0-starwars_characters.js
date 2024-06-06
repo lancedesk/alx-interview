b20bd9204f0d77a896ce4c78dc1af6c58cbe4cf6
Lance Desk <lncedesk@gmail.com>
2024-06-06 12:00:43 -0700

    A script that prints all characters of a Star Wars movie:

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,27 @@
 #!/usr/bin/node
-const fetch = require('node-fetch');
+/**
+ * Fetch Starwars Characters
+ */
+const request = require('request');
 
-// Get the movie ID from the command line arguments
-const movieId = process.argv[2];
-
-// Check if movieId is provided
-if (!movieId) {
-    console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-    process.exit(1);
+function getCharacter (urls)
+{
+  if (urls.length === 0)
+  {
+      return;
+  }
+  request.get(urls[0], (_error, _response, body) =>
+  {
+    console.log(JSON.parse(body).name);
+    getCharacter(urls.slice(1));
+  });
 }
 
-// API URL for the movie
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-async function fetchCharacters(url) {
-    try {
-        // Fetch the movie details
-        const response = await fetch(url);
-        const film = await response.json();
-
-        // Get the list of character URLs
-        const characters = film.characters;
-
-        // Fetch and print each character's name sequentially
-        for (const characterUrl of characters) {
-            const characterResponse = await fetch(characterUrl);
-            const character = await characterResponse.json();
-            console.log(character.name);
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-}
-
-// Call the function to fetch and print characters
-fetchCharacters(url);
+request.get(
+  'https://swapi-api.alx-tools.com/api/films/' + process.argv[2],
+  (_error, _response, body) =>
+  {
+    const characters = JSON.parse(body).characters;
+    getCharacter(characters);
+  }
+);

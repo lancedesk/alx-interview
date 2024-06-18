3b09144d855bed53a0cc975595e00738111bc9c6
Lance Desk <lncedesk@gmail.com>
2024-06-18 00:53:38 -0700

    A function to determine the fewest number of coins needed to meet a given amount total.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -23,12 +23,15 @@ def makeChange(coins, total):
 
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    coins.sort(reverse=True)
+    needed_coins = 0
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+        if total / coin > 0:
+            needed_coins = needed_coins + (total // coin)
+            total = total % coin
+        if not total:
+            break
 
-    return dp[total] if dp[total] != float('inf') else -1
+    if total != 0 or needed_coins == 0:
+        return -1
+    return needed_coins
